7d6b7620d86f92565d0ff49103df1042f0b90696
LuluDev1 <kinglewi786@gmail.com>
2024-09-17 22:26:36 -0500

    Need to add a backend and login function

---
+++ src/client/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { CiCirclePlus } from "react-icons/ci";
+import "./App.scss";
+
+function App() {
+  const [textarea, setTextArea] = useState("");
+
+  return (
+    <div className="App">
+      <h1>GroupMania Team Connect</h1>
+      <div className="searchbar">
+        <CiCirclePlus size={24} className="searchIcon" />
+        <div className="inputs">
+          <input type="file" name="" id="" accept="image/*" />
+          <textarea
+            name=""
+            id=""
+            placeholder="Share Today"
+            onChange={(e) => setTextArea(e.target.value)}
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
