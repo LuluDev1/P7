5eeed6ea25d82a2ebc8243594b2cf27ac34e69af
LuluDev1 <kinglewi786@gmail.com>
2024-09-27 16:59:33 -0500

    Finished designing the authentication form

--- src/client/pages/Login.jsx
+++ src/client/pages/Login.jsx
@@ -1,9 +1,24 @@
+import "../styles/Login.scss";
+import image from "../assets/icon-left-font-monochrome-white.webp";
 import React from "react";
 
 const Login = () => {
   return (
     <div className="loginPage">
-      <h1>Hello</h1>
+      <div className="form_container">
+        <img src={image} alt="" className="logo" />
+        <form action="">
+          <div className="input_container">
+            <input type="email" name="" id="" placeholder="Email" />
+          </div>
+          <div className="input_container">
+            <input type="password" name="" id="" placeholder="password" />
+          </div>
+          <button id="login_btn">Login</button>
+
+          <button id="sign_up">Sign up</button>
+        </form>
+      </div>
     </div>
   );
 };
